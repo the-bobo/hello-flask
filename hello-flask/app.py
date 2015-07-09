0a3bee066dba3fa80180562cf404d7533f0c10cc
Kai Chen <kaichen@mathildetech.com>
2015-07-08 17:45:56 -0700

    demo prep

--- hello-flask/app.py
+++ hello-flask/app.py
@@ -1,10 +1,9 @@
 from flask import Flask
-import socket
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Bobo, from {}!\n'.format(socket.gethostname())
+    return 'Hello, World!\n'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
