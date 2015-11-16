a015f5f634a179b008ef5d8cad846e784047eed4
Bobo <the.bobo@gmail.com>
2015-11-16 15:50:08 -0500

    testing auto-build - changing edit so it will show up on webpage

--- hello-flask/app.py
+++ hello-flask/app.py
@@ -3,8 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, world!\n'
-    return 'This is an edit!\n'
+    return 'Hello, world!\n This is an edit!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
